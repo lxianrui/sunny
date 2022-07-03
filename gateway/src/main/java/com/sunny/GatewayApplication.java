37e044fc3b946f7606e43da7a54162658f57b5e3
Jeffery Liu <jeff16@foxmail.com>
2022-07-03 23:54:04 +0800

    init project

---
+++ gateway/src/main/java/com/sunny/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.sunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关应用程序
+ *
+ * @author Jeffrey Liu
+ * @date 2022/07/03
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
